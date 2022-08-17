a505e4357a8829482a471fc0eb2cb9c74b543965
christopher-moses <christmoses1999@icloud.com>
2022-08-18 00:35:49 +0100

    username-updata

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,76 @@
-import './App.css';
-const App = () => {
-  return 
+import { useState } from "react"
+import "./App.css";
+import FormInput from './components/Formlnput';
 
+const App = () => {
+  const [value, setValue] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text", 
+      placeholder: "Username",
+      label: "username" 
+    },
+
+     {
+      id: 2,
+      name: "email",
+      type: "text", 
+      placeholder: "Email",
+      label: "Email" 
+    },
+
+      {
+      id: 3,
+      name: "birthday",
+      type: "text", 
+      placeholder: "Birthday",
+      label: "Birthday" 
+    },
+      
+       {
+      id: 4,
+      name: "password",
+      type: "password", 
+      placeholder: "Password",
+      label: "Password" 
+    },
+       
+        {
+      id: 5,
+      name: "confirmPassword",
+      type: "password", 
+      placeholder: "Confirm Password",
+      label: "Confirm Password" 
+    }
+     
+  ]
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const data = new FormData(e.target)
+    // console.log(Object.fromEntries(data.entries()));
+  };
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput key={input.id} name="username" placeholder="Username" />
+       ))}
+        <button>Submit</button>
+      </form>
+    
+    </div>
+  );
 };
 
 export default App;
